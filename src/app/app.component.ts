484fc0a40a2eff4ee8c661a6694d116ca4d73db1
Kursovik <beschastnovaleks@gmail.com>
2023-02-05 20:47:01 +0300

    Create toDo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ModalServiceService } from './services/modal-service.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test-to-do';
+export class AppComponent  {
+  //Название проекта
+  title = 'to-do list'
+  constructor(
+    public modalService : ModalServiceService
+  ){}
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './components/task/task.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SearchTaskPipe } from './pipes/search-task.pipe';
+import { CreateModalComponent } from './components/modal/create-modal/create-modal.component';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
+import { EditTaskComponent } from './components/edit-task/edit-task.component';
+import { EditModalComponent } from './components/modal/edit-modal/edit-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskComponent,
+    SearchTaskPipe,
+    CreateModalComponent,
+    CreateTaskComponent,
+    EditTaskComponent,
+    EditModalComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
